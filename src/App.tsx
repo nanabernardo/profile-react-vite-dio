504d7cfba4f2539097d79ef4e40d8ac7b00fd978
nanabernardo <nanabernardo@gmail.com>
2025-09-26 21:59:07 -0300

    feat: add Profile and Job components to App with named export

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import "./App.css";
+import { Profile, Job } from "./components/Profile";
 
 function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <div>
+      <Profile />
+      <Job />
+    </div>
+  );
 }
 
 export default App;
--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,8 +1,48 @@
-
-function Profile() {
+export function Profile() {
   return (
     <div>
-      <h1>Profile</h1>
+      <img
+        src="https://media.licdn.com/dms/image/v2/D4D03AQGhsMwBUFZdUQ/profile-displayphoto-crop_800_800/B4DZejTAFeGgAI-/0/1750791358253?e=1761782400&v=beta&t=RCXuIc0zR4uBTOQ_Ex1fubShLfph_l-TILKILg3dn60"
+        alt="Mariana Bernardo"
+      />
+      <h3>Mariana Bernardo da Rocha</h3>
+      <p>Junior Front-end Developer</p>
+      <p>
+        Em transição para a área de desenvolvimento de software, com foco em
+        desenvolvimento front-end. Tenho me dedicado ao estudo de linguagens e
+        frameworks como HTML, CSS, JavaScript, React, Next.js e Python, buscando
+        aplicar minhas habilidades analíticas e atenção aos detalhes na criação
+        de soluções digitais eficientes e inovadoras.
+      </p>
+      <p>
+        Com uma forte base em resolução de problemas e uma mentalidade voltada
+        para dados, estou entusiasmada para aplicar meus conhecimentos em um
+        novo contexto e contribuir para equipes de desenvolvimento ágeis. Estou
+        aberta a novas oportunidades que me permitam combinar minha experiência
+        científica com as tecnologias emergentes do mundo do desenvolvimento
+        web.
+      </p>
+      <p>
+        Sou graduada em Ciências Biólogicas pela Unicamp e tese de mestrado
+        defendida pela Unifesp. Tive uma sólida experiência em pesquisa durante
+        minha jornada acadêmica, tendo trabalhado com análise de sequências
+        genéticas de rotavírus e HIV, bem como bioinformática.
+      </p>
+    </div>
+  );
+}
+
+export function Job() {
+  return (
+    <div>
+      <h4>Freelancer</h4>
+      <p>
+        Desenvolvimento de interfaces responsivas utilizando HTML5, CSS e
+        JavaScript. Criação de páginas institucionais e aplicações front-end
+        dinâmicas com React.js, incluindo integração com APIs e atenção a UI/UX
+        para garantir usabilidade e acessibilidade. Atuação em ajustes de
+        layout, manutenção de código e otimização de desempenho em projetos web.
+      </p>
     </div>
   );
 }
